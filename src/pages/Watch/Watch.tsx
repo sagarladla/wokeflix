70bf3ec8667644d4a7f2815b9b0ee8835fc22852
Sagar Ladla <sagarladla@gmail.com>
2021-10-28 01:25:31 +0530

    added classname to video element

--- src/pages/Watch/Watch.tsx
+++ src/pages/Watch/Watch.tsx
@@ -1,16 +1,19 @@
-import { ArrowBackOutlined } from "@mui/icons-material"
-import "./watch.scss"
+import { ArrowBackOutlined } from "@mui/icons-material";
+import "./watch.scss";
 
-export default function Watch(params:any): JSX.Element {
-    return (
-        <div className="watch">
-            <div className="back">
-                <ArrowBackOutlined />
-                Home
-            </div>
-            <video controls autoPlay={true}>
-                <source src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm" type="video/webm" />
-            </video>
-        </div>
-    )
+export default function Watch(params: any): JSX.Element {
+  return (
+    <div className="watch">
+      <div className="back">
+        <ArrowBackOutlined />
+        Home
+      </div>
+      <video className="video" controls autoPlay={true}>
+        <source
+          src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm"
+          type="video/webm"
+        />
+      </video>
+    </div>
+  );
 }
